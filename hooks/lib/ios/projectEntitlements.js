8f50849b08110390522ca3dff3937fbb1d392f35
Justin Hill <justin@comfyapp.com>
2017-05-02 13:13:59 -0700

    monkey patch for entitlements

--- hooks/lib/ios/projectEntitlements.js
+++ hooks/lib/ios/projectEntitlements.js
@@ -5,172 +5,83 @@ Location: ProjectName/
 
 Script only generates content. File it self is included in the xcode project in another hook: xcodePreferences.js.
 */
-(function() {
 
-  var path = require('path'),
-    fs = require('fs'),
-    plist = require('plist'),
-    mkpath = require('mkpath'),
-    ConfigXmlHelper = require('../configXmlHelper.js'),
-    ASSOCIATED_DOMAINS = 'com.apple.developer.associated-domains',
-    context,
-    projectRoot,
-    projectName,
-    entitlementsFilePath;
+var path = require('path');
+var fs = require('fs');
+var plist = require('plist');
+var mkpath = require('mkpath');
+var context;
 
-  module.exports = {
-    generateAssociatedDomainsEntitlements: generateEntitlements
-  };
+module.exports = {
+  generateAssociatedDomainsEntitlements: generateEntitlements
+};
 
-  // region Public API
+// region Public API
 
-  /**
-   * Generate entitlements file content.
-   *
-   * @param {Object} cordovaContext - cordova context object
-   * @param {Object} pluginPreferences - plugin preferences from config.xml; already parsed
-   */
-  function generateEntitlements(cordovaContext, pluginPreferences) {
-    context = cordovaContext;
+/**
+ * Generate entitlements file content.
+ *
+ * @param {Object} context - cordova context object
+ * @param {Object} pluginPreferences - plugin preferences from config.xml; already parsed
+ */
+function generateEntitlements(context, pluginPreferences) {
+    var iosPlatform = path.join(context.opts.projectRoot, 'platforms/ios/');
+    var iosFolder = fs.existsSync(iosPlatform) ? iosPlatform : context.opts.projectRoot;
 
-    var currentEntitlements = getEntitlementsFileContent(),
-      newEntitlements = injectPreferences(currentEntitlements, pluginPreferences);
-
-    saveContentToEntitlementsFile(newEntitlements);
-  }
-
-  // endregion
-
-  // region Work with entitlements file
-
-  /**
-   * Save data to entitlements file.
-   *
-   * @param {Object} content - data to save; JSON object that will be transformed into xml
-   */
-  function saveContentToEntitlementsFile(content) {
-    var plistContent = plist.build(content),
-      filePath = pathToEntitlementsFile();
-
-    // ensure that file exists
-    mkpath.sync(path.dirname(filePath));
-
-    // save it's content
-    fs.writeFileSync(filePath, plistContent, 'utf8');
-  }
-
-  /**
-   * Read data from existing entitlements file. If none exist - default value is returned
-   *
-   * @return {String} entitlements file content
-   */
-  function getEntitlementsFileContent() {
-    var pathToFile = pathToEntitlementsFile(),
-      content;
-
-    try {
-      content = fs.readFileSync(pathToFile, 'utf8');
-    } catch (err) {
-      return defaultEntitlementsFile();
+    var data = fs.readdirSync(iosFolder);
+    var projFolder = null;
+    var projName = null;
+    if (data && data.length) {
+        data.forEach(function (folder) {
+            if (folder.match(/\.xcodeproj$/)) {
+                projName = path.basename(folder, '.xcodeproj');
+                projFolder = path.join(iosFolder, projName);
+            }
+        });
     }
 
-    return plist.parse(content);
-  }
-
-  /**
-   * Get content for an empty entitlements file.
-   *
-   * @return {String} default entitlements file content
-   */
-  function defaultEntitlementsFile() {
-    return {};
-  }
-
-  /**
-   * Inject list of hosts into entitlements file.
-   *
-   * @param {Object} currentEntitlements - entitlements where to inject preferences
-   * @param {Object} pluginPreferences - list of hosts from config.xml
-   * @return {Object} new entitlements content
-   */
-  function injectPreferences(currentEntitlements, pluginPreferences) {
-    var newEntitlements = currentEntitlements,
-      content = generateAssociatedDomainsContent(pluginPreferences);
-
-    newEntitlements[ASSOCIATED_DOMAINS] = content;
-
-    return newEntitlements;
-  }
-
-  /**
-   * Generate content for associated-domains dictionary in the entitlements file.
-   *
-   * @param {Object} pluginPreferences - list of hosts from conig.xml
-   * @return {Object} associated-domains dictionary content
-   */
-  function generateAssociatedDomainsContent(pluginPreferences) {
-    var domainsList = [],
-      link;
-
-    // generate list of host links
-    pluginPreferences.hosts.forEach(function(host) {
-      link = domainsListEntryForHost(host);
-      domainsList.push(link);
-    });
-
-    return domainsList;
-  }
-
-  /**
-   * Generate domain record for the given host.
-   *
-   * @param {Object} host - host entry
-   * @return {String} record
-   */
-  function domainsListEntryForHost(host) {
-    return 'applinks:' + host.name;
-  }
-
-  // endregion
-
-  // region Path helper methods
-
-  /**
-   * Path to entitlements file.
-   *
-   * @return {String} absolute path to entitlements file
-   */
-  function pathToEntitlementsFile() {
-    if (entitlementsFilePath === undefined) {
-      entitlementsFilePath = path.join(getProjectRoot(), 'platforms', 'ios', getProjectName(), 'Resources', getProjectName() + '.entitlements');
+    if (!projFolder || !projName) {
+        throw new Error("Could not find an .xcodeproj folder in: " + iosFolder);
     }
 
-    return entitlementsFilePath;
-  }
+    if (directoryExists(iosFolder)) {
+      ['Debug', 'Release'].forEach(function(target) {
+          var pathToFile = path.join(projFolder, 'Entitlements-' + target + '.plist');
+          var entitlements;
 
-  /**
-   * Projects root folder path.
-   *
-   * @return {String} absolute path to the projects root
-   */
-  function getProjectRoot() {
-    return context.opts.projectRoot;
-  }
+          try {
+              entitlements = plist.parse(fs.readFileSync(pathToFile, 'utf8'));
+          } catch (err) {
+              entitlements = {};
+          }
 
-  /**
-   * Name of the project from config.xml
-   *
-   * @return {String} project name
-   */
-  function getProjectName() {
-    if (projectName === undefined) {
-      var configXmlHelper = new ConfigXmlHelper(context);
-      projectName = configXmlHelper.getProjectName();
+          var domainsList = [];
+          pluginPreferences.hosts.forEach(function(host) {
+              var link = 'applinks:' + host.name;
+              if (domainsList.indexOf(link) === -1) {
+                  domainsList.push(link);
+              }
+          });
+
+          entitlements['com.apple.developer.associated-domains'] = domainsList;
+
+          // save it's content
+          fs.writeFileSync(pathToFile, plist.build(entitlements), 'utf8');
+      });
     }
+}
 
-    return projectName;
-  }
+// endregion
 
-  // endregion
+// region Path helper methods
 
-})();
+function directoryExists(path) {
+    try  {
+        return fs.statSync(path).isDirectory();
+    } catch (e) {
+        logMe("directoryExists error: " + e);
+        return false;
+    }
+}
+
+// endregion
